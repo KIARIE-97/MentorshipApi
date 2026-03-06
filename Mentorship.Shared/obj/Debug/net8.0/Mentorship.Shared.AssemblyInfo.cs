f41d01b1e06e09e52a44fb2e2d3692d6973c1d2a
sarah kiarie <sarahwanjiruki1@gmail.com>
2026-03-06 14:16:57 +0300

    am on main about to screw-up haha

--- Mentorship.Shared/obj/Debug/net8.0/Mentorship.Shared.AssemblyInfo.cs
+++ Mentorship.Shared/obj/Debug/net8.0/Mentorship.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mentorship.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8acbfd732df920c8905814678ca7a1656a8472eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mentorship.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mentorship.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
