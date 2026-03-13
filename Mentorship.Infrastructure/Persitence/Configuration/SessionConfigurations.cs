d092cb5cbc9cc3a048b019a050c06238d214b697
sarah kiarie <sarahwanjiruki1@gmail.com>
2026-03-13 10:36:35 +0300

    session configuration done

--- Mentorship.Infrastructure/Persitence/Configuration/SessionConfigurations.cs
+++ Mentorship.Infrastructure/Persitence/Configuration/SessionConfigurations.cs
@@ -1,8 +1,45 @@
 using System;
+using Mentorship.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Mentorship.Infrastructure.Persitence.Configuration;
 
-public class SessionConfigurations
+public class SessionConfigurations: IEntityTypeConfiguration<Session>
 {
+    public void Configure(EntityTypeBuilder<Session> builder)
+    {
+        builder.ToTable("Sessions");
+        
+        builder.HasKey(s=>s.Id);
+
+        builder.Property(p => p.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+            
+        builder.Property(p => p.Description)
+            .HasMaxLength(2000);
+         builder.Property(s => s.Sessiontype)
+            .IsRequired()
+            .HasConversion<int>();  // Store enum as int in database
+            
+        builder.Property(s => s.ScheduleAt)
+            .IsRequired()
+            .HasColumnType("timestamp with time zone");  // PostgreSQL specific
+            
+        builder.Property(s => s.DurationMinutes)
+            .IsRequired();
+            
+        // 4. Foreign Key relationship
+        builder.HasOne(s => s.Program)
+            .WithMany(p => p.Sessions)
+            .HasForeignKey(s => s.ProgramId)
+            .OnDelete(DeleteBehavior.Cascade);  // Delete sessions when program is deleted
+            
+        // 5. Indexes for performance
+        builder.HasIndex(s => s.ProgramId);  // Fast lookup by program
+        builder.HasIndex(s => s.ScheduleAt); // Fast date-based queries
+        builder.HasIndex(s => s.Sessiontype); // Fast filter by type    
+    }
 
 }
--- Mentorship.Infrastructure/obj/Debug/net8.0/Mentorship.Infrastructure.AssemblyInfo.cs
+++ Mentorship.Infrastructure/obj/Debug/net8.0/Mentorship.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mentorship.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ebdb328e52d92b37b6d69e408cb3e56a384b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102d87db4eb39c9ccfb9f82bd27f853334b936d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mentorship.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mentorship.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
